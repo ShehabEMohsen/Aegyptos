c277872f673161cc75ea7d27c3e5286844dd9e1d
reemansour <mansourreem02gmail.com>
2023-03-22 14:47:11 +0200

    matching algoritm modified

--- matching_algorithm_pronunciation.py
+++ matching_algorithm_pronunciation.py
@@ -3,7 +3,7 @@
 def dictionary_matching_pronunciation(top1):#A040
     str_list_pronunce={
         'پر':"O001",'پرى':"D021",'بري':"O001 D021 D054","پرت":'O001 D021 X001 N005','جر':"W011 D021 A002",'نيس':"T035 S029 N035 A002",
-        'ممي':'G018 Z004 F027',
+        'ممي':'G018 Z004 F027', 'بري':"D021 O001 D055"
     }
     
     key_list_pronunciation=list(str_list_pronunce.keys())
