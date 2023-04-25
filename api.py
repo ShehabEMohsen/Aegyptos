cc2ed06f67a239b95eac20c43cc58a730572cae5
reemansour <mansourreem0@gmail.com>
2023-04-25 17:34:58 +0200

    camera error fixed

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import numpy as np
 from flask import Flask, request, jsonify, render_template
@@ -22,40 +23,9 @@ model = pickle.load(open("new squeezenet(finetuned).pkl", "rb"))
 
 image_array = []
 
-# def Otsu(myImage):
-#   sample_image = cv2.imread(myImage)
-#   img = cv2.cvtColor(sample_image,cv2.COLOR_BGR2RGB)
-
-#   # plt.axis('off')
-#   # plt.imshow(img)
-
-#   img_gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-
-#   thresh = threshold_otsu(img_gray)
-#   img_otsu  = img_gray < thresh
-
-#   # plt.imshow(img_otsu)
-
-#   filtered = filter_image(img, img_otsu)
-#   filteredBW=cv2.cvtColor(filtered,cv2.COLOR_RGB2GRAY)
-#   return filteredBW
-
-
-
-# def filter_image(image, mask):
-
-#     r = image[:,:,0] * mask
-#     g = image[:,:,1] * mask
-#     b = image[:,:,2] * mask
-
-#     return np.dstack([r,g,b])
-# global deleteCounterImage
-# deleteCounterImage=0
 def cropping(myImage):
     global deleteCounterImage 
-    deleteCounterImage = 0  # default value
-    # Load the YOLOv3 network
-    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    deleteCounterImage = 0
 
     # Set confidence threshold and non-maximum suppression threshold for YOLO
     conf_threshold = 0.5
@@ -73,72 +43,42 @@ def cropping(myImage):
     # Find contours
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # binary_threshold = 50
+    blob_min_area = 100
+    # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('gray.png', gray)
+    # Apply Otsu's thresholding method to get a binary image
+    _, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Invert the binary image
+    binary = cv2.bitwise_not(threshold)
+    cv2.imwrite('binary.png', binary)
+
+    # Perform blob analysis
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Iterate over the contours
     for i, contour in enumerate(contours):
-        deleteCounterImage=deleteCounterImage+1
-        # Get bounding box coordinates
-        x, y, w, h = cv2.boundingRect(contour)
+        # deleteCounterImage = deleteCounterImage+1
+        # Check if the area of the contour is large enough to be considered an object
+        if cv2.contourArea(contour) > blob_min_area:
+            # Get bounding box coordinates
+            x, y, w, h = cv2.boundingRect(contour)
 
-        # Crop the image using the bounding box coordinates
-        crop_img = image[y:y+h, x:x+w]
-
-        # Create a blob from the cropped image
-        blob = cv2.dnn.blobFromImage(crop_img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-
-        # Pass the blob through the network to perform object detection
-        net.setInput(blob)
-        layer_names = net.getLayerNames()
-        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-        outputs = net.forward(output_layers)
-
-        boxes = []
-        confidences = []
-
-        # Iterate over the output layers
-        for output in outputs:
-            # Iterate over the detected objects
-            for detection in output:
-                # Extract the class ID and confidence score
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-
-                # Check if the detected object is above the confidence threshold
-                if confidence > conf_threshold:
-                    # Compute the bounding box coordinates of the detected object
-                    center_x = int(detection[0] * w)
-                    center_y = int(detection[1] * h)
-                    box_width = int(detection[2] * w)
-                    box_height = int(detection[3] * h)
-                    left = int(center_x - box_width / 2)
-                    top = int(center_y - box_height / 2)
-
-                    # Apply non-maximum suppression to remove overlapping boxes
-                    box = [left, top, box_width, box_height]
-                    
-                    boxes.append(box)
-                    confidences.append(float(confidence))
+            # Crop the image using the bounding box coordinates
+            crop_img = image[y:y+h, x:x+w]
+            # Save the cropped image to a file
+            
+            filename = f"crop_{i}.png"
+            cv2.imwrite(filename, crop_img)
         
-        # Apply non-maximum suppression to remove overlapping boxes
-        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-        # Draw the final bounding boxes on the cropped image
-        for i in indices:
-            i = i[0]
-            box = boxes[i]
-            x, y, w, h = box
-            cv2.rectangle(crop_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Save the cropped image to a file
-        filename = f"crop_{i}.png"
-        cv2.imwrite(filename, crop_img)
-        
-
-    for i, contour in enumerate(contours):
-        print("bassem")
+    image_files = glob.glob("*crop*.png")
+    for i, image_file in enumerate(image_files):
+        # print("bassem")
         # Load the input image
-        image = cv2.imread(f"crop_{i}.png")
+        
+        image = cv2.imread(image_file)
 
         # Get the original dimensions of the image
         height, width, _ = image.shape
@@ -194,30 +134,8 @@ def cropping(myImage):
 
         # Save the final image to a file
         cv2.imwrite(f"final{i}.png", new_image)
-
+        deleteCounterImage = deleteCounterImage+1
         image_array.append(new_image)
-
-        # # Create a new blank image with the desired output size
-        # new_image = np.zeros((output_size, output_size, 3), np.uint8)
-        # new_image[:, :] = (255, 255, 255)  # fill with black pixels
-
-        # # Calculate the coordinates to copy the resized image onto the new image
-        # x_offset = int((output_size - new_width) / 2)
-        # y_offset = int((output_size - new_height) / 2)
-
-        # # Copy the resized image onto the new image with black pixels
-        # new_image[y_offset:y_offset+new_height, x_offset:x_offset+new_width] = resized_image
-
-        # # Save the final image to a file
-        # cv2.imwrite(f"resize{i}.png", new_image)
-
-        # image = cv2.imread(f"resize{i}.png")
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
-        # inv = cv2.bitwise_not(thresh)
-        # seg_img = cv2.imwrite(f"seg_img{i}.png", inv)
-        # image_array.append(seg_img)
-
 
 
 @app.route('/predict', methods=['POST'])
@@ -283,8 +201,9 @@ def predict():
         matchingTextPronunciation=pronunciation.dictionary_matching_pronunciation(updated_results)
         
         os.remove(filename)
-        for i in range(deleteCounterImage):
-            removeCrop = f"crop_{i}.png"
+        cropImageRemovals = glob.glob("*crop*.png")
+        for i,cropImageRemoval in enumerate(cropImageRemovals):
+            removeCrop = cropImageRemoval
             removeSegImg = f"seg_img{i}.png"
             removeFinal = f"final{i}.png"
             removeResize = f"resize{i}.png"
